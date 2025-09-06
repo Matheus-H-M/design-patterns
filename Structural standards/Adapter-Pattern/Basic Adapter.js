ae434fc6425b58d12034819492a07685ce096a22
Matheus Henrique <matheushmv.97@gmail.com>
2025-09-06 17:37:41 -0300

    add Basic adapter

--- Structural standards/Adapter-Pattern/Basic Adapter.js
+++ Structural standards/Adapter-Pattern/Basic Adapter.js
@@ -1,17 +1,27 @@
-class OldPrinter{
-    printText(text){
+// Classe antiga que queremos adaptar
+class OldPrinter {
+    // Método da classe antiga
+    printText(text) {
         console.log('Imprimindo:', text);
     }
 }
 
-class PrinterAdapter{
-    constructor(){
+// Classe adaptadora que converte a interface antiga para a nova
+class PrinterAdapter {
+    constructor() {
+        // Cria uma instância da classe antiga
         this.oldPrinter = new OldPrinter();
     }
 
-    print(message){
+    // Novo método que os clientes modernos usarão
+    print(message) {
+        // Internamente, ainda usa o método antigo
         this.oldPrinter.printText(message);
     }
 }
+
+// Cliente usando a nova interface
 const printer = new PrinterAdapter();
+
+// Chamada feita com a nova interface, mas que usa a lógica antiga por trás
 printer.print('Olá mundo');
