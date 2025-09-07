7e264c856d361db624de0bd011c520e56219d8e0
Matheus Henrique <matheushmv.97@gmail.com>
2025-09-07 16:12:37 -0300

    Atualiza Design-Patterns-Prototype e Adapter para diferentes APIs

--- Structural standards/Adapter-Pattern/Adapter for different APIs.js
+++ Structural standards/Adapter-Pattern/Adapter for different APIs.js
@@ -1,23 +1,6 @@
-// Legacy class with an old method
-class legacyAPI {
-    // Old method that performs some action
-    doAction() {
-        return "Executing old action";
-    }
-}
-
-// Adapter class that adapts the legacy API to a modern interface
-class ModernAdapter {
-    constructor() {
-        // Creating an instance of the legacy API
-        this.legacy = new legacyAPI();
-    }
-
-    // Modern method that delegates to the legacy method
-    execute() {
-        return this.legacy.doAction();
-    }
-}
-
-// Testing the adapter: it will call the legacy method through the modern interface
-console.log(new ModernAdapter().execute());
+const baseObject = {
+    created: new Date(),
+    getTime() { return this.created.getTime(); }
+};
+const obj1 = Object.create(baseObject);
+console.log(obj1.getTime());
