d3682ead4d70e2aaabda29c003e3cf5ccabe4657
Matheus Henrique <matheushmv.97@gmail.com>
2025-09-07 16:25:05 -0300

    Descreva aqui as alterações feitas

--- Structural standards/Adapter-Pattern/Adapter for different APIs.js
+++ Structural standards/Adapter-Pattern/Adapter for different APIs.js
@@ -1,6 +1,14 @@
+// Define um objeto base com uma propriedade 'created' e um método 'getTime'
 const baseObject = {
+    // A propriedade 'created' armazena a data e hora atuais no momento da criação do objeto
     created: new Date(),
+    // Método que retorna o timestamp da data armazenada em 'created'
     getTime() { return this.created.getTime(); }
 };
+
+// Cria um novo objeto 'obj1' que herda do 'baseObject'
 const obj1 = Object.create(baseObject);
+
+// Chama o método 'getTime' de 'obj1' e exibe o resultado no console
+// Como 'obj1' herda de 'baseObject', ele acessa o método e a propriedade 'created'
 console.log(obj1.getTime());
