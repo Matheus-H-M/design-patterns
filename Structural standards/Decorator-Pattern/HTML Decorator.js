63d16895d8d42371f6c71b8d14b39b11d701c02b
Matheus Henrique <matheushmv.97@gmail.com>
2025-09-23 15:41:02 -0300

    Add HTML Decorator

---
+++ Structural standards/Decorator-Pattern/HTML Decorator.js
@@ -0,0 +1,8 @@
+// Define a function named decorateText that takes one parameter called 'text'
+function decorateText(text){
+    // Return the input text wrapped in <strong> HTML tags (used to make text bold)
+    return `<strong>${text}</strong>`;
+}
+
+// Log the string "Importante" to the console
+console.log("Importante");
