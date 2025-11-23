dad1f80ecd321c5cc24134da0e35b5799c387989
Matheus Henrique <matheushmv.97@gmail.com>
2025-11-23 14:07:08 -0300

    add change

--- Behavioral patterns/Command-Pattern/Timer command.js
+++ Behavioral patterns/Command-Pattern/Timer command.js
@@ -1,13 +1,19 @@
-class ComandoAtrasado{
-    constructor(mensagem, delay){
-        this.mensagem = mensagem;
-        this.delay = delay;
+// Class that represents a delayed command
+class ComandoAtrasado {
+    // Constructor receives a message and a delay time in milliseconds
+    constructor(mensagem, delay) {
+        this.mensagem = mensagem; // Stores the message
+        this.delay = delay;       // Stores the delay
     }
-    executar(){
+
+    // Method that executes the command after the given delay
+    executar() {
         setTimeout(() => {
-            console.log(this.mensagem);
-        }, this.delay);
+            console.log(this.mensagem); // Prints the message to the console
+        }, this.delay); // Time to wait before executing
     }
 }
-new ComandoAtrasado("Executando depois de 2s",
-     2000).executar();
+
+// Creates a new delayed command and executes it
+// In this case, it prints the message after 2 seconds (2000 ms)
+new ComandoAtrasado("Executando depois de 2s", 2000).executar();
